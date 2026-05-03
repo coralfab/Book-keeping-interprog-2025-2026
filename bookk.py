510c507b0510c89f91ae66945d1030c728ed12ab
Nuella Yzabelle Alvarez <nuella.alvarez2007@gmail.com>
2026-05-03 17:03:07 +0800

    Added transaction history in bookk.py

--- bookk.py
+++ bookk.py
@@ -4,7 +4,11 @@ print("   2. View transaction history")
 print("   3. Remove transaction record")
 print("   4. Exit program")
 
+transaction = ""
+
 def record_transaction():
+    global transaction
+    
     calendar = input("\nDate of transaction (YY/MM/DD): ")
     type_money = input("What type of transaction (Expense/Income): ")
     description = input("Description about transaction: ")
@@ -21,9 +25,21 @@ def record_transaction():
     print("Type:", type_money)
     print("Description:", description)
     print("Amount:", money)
-
+    
+    transaction += f"{calendar}, {type_money}, {description}, {money}\n"
+    
 while True:
     choice = int(input("\nChoose a number (1 - 4): "))
 
     if choice == 1:
         record_transaction()
+    elif choice == 2:
+        print("----- Transaction History ----- ")
+        
+        if not transaction:
+            print("No transaction made yet")
+        else:
+            record = transaction.strip().split("\n")
+
+            for index, number in enumerate(record, start=1):
+                print(f"{index}. {number}")
